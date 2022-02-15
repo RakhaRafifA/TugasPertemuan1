d80d805d170820036528a60aff1295c009ffd478
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-15 14:50:28 +0700

    Tugas Pertemuan 1

---
+++ Pertemuan1_RakhaRA.java
@@ -0,0 +1,11 @@
+class rakhara{
+    public static void main(String[] args) {
+
+        System.out.println("Tugas Pertemuan 1 Rakha Rafif Arifin");
+        System.out.println("Alterra Academy");
+
+        System.out.println("A");
+        System.out.println("B");
+        
+    }
+}
